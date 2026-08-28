275eebe87a302609e6bfb39deb8a2bcc29630516
이재호 <ljh2735294@nver.com>
2026-08-28 23:28:08 +0900

    Frontned / Mobile: ReactQuery 프로바이더 추가

--- frontend/mobile/app/_layout.tsx
+++ frontend/mobile/app/_layout.tsx
@@ -2,11 +2,14 @@ import '../global.css';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { Stack } from 'expo-router';
+import ReactQueryProvider from '@/providers/ReactQuery.provider';
 
 export default function Layout() {
     return (
-        <SafeAreaProvider>
-            <Stack />
-        </SafeAreaProvider>
+        <ReactQueryProvider>
+            <SafeAreaProvider>
+                <Stack />
+            </SafeAreaProvider>
+        </ReactQueryProvider>
     );
-} 
+}
