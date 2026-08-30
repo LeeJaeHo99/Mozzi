4421d64715f47a3f7d1341f0702a6da585fc06e8
이재호 <ljh2735294@nver.com>
2026-08-30 18:59:45 +0900

    User 엔티티 설계

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,26 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from 'src/user/entities/user.entity';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
-  }
-
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
-  }
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+    ){}
 }
--- backend/src/user/entities/user.entity.ts
+++ backend/src/user/entities/user.entity.ts
@@ -1,1 +1,42 @@
-export class User {}
+import { Column, Entity } from "typeorm";
+import { Exclude } from 'class-transformer';
+import { Base } from "src/base/entities/Base.entity";
+import { Gender, Role } from "../enums/user.enum";
+
+@Entity()
+export class User extends Base{
+    @Column({ type: 'enum', enum: Role })
+    role!: Role;
+
+    @Column()
+    email!: string;
+
+    @Column()
+    nickname!: string;
+
+    @Column()
+    hiddenNickname!: string;
+
+    @Column()
+    description!: string;
+
+    @Column({ type: 'enum', enum: Gender})
+    gender!: Gender;
+
+    @Column({ type: 'varchar', nullable: true })
+    profileImgUrl!: string | null;
+
+    @Column()
+    hiddenProfileEmoji!: string;
+
+    @Column({ type: 'date' })
+    birthday!: string;
+
+    @Exclude({ toPlainOnly: true })
+    @Column()
+    provider!: string;
+    
+    @Exclude({ toPlainOnly: true })
+    @Column()
+    providerId!: string;
+}
